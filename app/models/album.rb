74ac5ab33cc11afd0af23b52963257d1b8d065f1
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-07-24 14:05:39 -0300

    Barra de pesquisa

--- app/models/album.rb
+++ app/models/album.rb
@@ -8,16 +8,10 @@ class Album < ApplicationRecord
   has_one_attached :photo
 
   def self.search(search)
-    if search
-      catapimbas = User.where(name: search).first
-      artista = Artist.where(user_id: catapimbas.id)
-      if artista
-        self.where(artist_id: artista)
-      else
-        Album.all
-      end
+    if search.present?
+      Album.where('name LIKE ?', "%#{search}%")
     else
       Album.all
-    end
+     end
   end
 end
